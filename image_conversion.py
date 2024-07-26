87f4a00958a617ad191a3559b095018d6e330ec2
nadeemohc <nadeemvezhappilly@gmail.com>
2024-07-26 11:57:43 +0530

    added the jpeg to png conversion

--- image_conversion.py
+++ image_conversion.py
@@ -15,6 +15,16 @@ def jpg_to_png(directory, input_extension, output_format):
             print(f'[bold green]Converted {filename} to {output_filename}[/]')
     print('[bold white]PNG conversion completed[/]')
 
+def jpeg_to_png(directory, input_extension, output_format):
+    os.chdir(directory)
+    for filename in os.listdir(directory):
+        if filename.endswith(input_extension):
+            output_filename = os.path.splitext(filename)[0] + output_format
+            command = ['ffmpeg', '-i', filename, output_filename]
+            subprocess.run(command)
+            print(f'[bold green]Converted {filename} to {output_filename}[/]')
+    print('[bold white]PNG conversion completed[/]')
+
 def main():
     print('[bold green]Welcome to the[/][bold red][underline] Image Converter[/][/]')
     directory = Prompt.ask('[bold yellow]Enter the path of your[/][bold red][underline] Image Folder[/][/]')
@@ -26,12 +36,17 @@ def main():
     
     print("[bold green]Select conversion format:[/]")
     print("[bold white]1. [/][bold green][underline].jpg to .png[/][/]")
+    print("[bold white]2. [/][bold green][underline].jpeg to .png[/][/]")
     choice = Prompt.ask("[bold yellow]Enter your choice: [/]").strip()
 
     if choice == '1':
         input_extension = '.jpg'
         output_format = '.png'
         jpg_to_png(directory, input_extension, output_format)
+    elif choice == '2':
+        input_extension = '.jpeg'
+        output_format = '.png'
+        jpeg_to_png(directory, input_extension, output_format)    
     else:
         print('[bold green]Invalid choice.[/]')
         time.sleep(2)
